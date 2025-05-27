a570d5b8f629d4d688af45071f1a14b67cb9ef34
David-396 <david.eini12@gmail.com>
2025-05-27 11:25:09 +0300

    feature 7 - testing

--- Commando/Program.cs
+++ Commando/Program.cs
@@ -10,12 +10,21 @@ namespace Commando
     {
         static void Main(string[] args)
         {
-            Commando cmndo = new Commando("david", "123", new string[5] {"gun","knife","bag","hammer","grenade"}, StatusType.standing);
-            cmndo.Attack();
-            cmndo.Walk();
-            Weapon wpn = new Weapon("s100", "elvit", 5);
-            wpn.Shoot();
-            cmndo.CodeName = "456";
+            Commando cmndo1 = new Commando("david", "123", new string[5] {"gun","knife","bag","hammer","grenade"}, StatusType.standing);
+            Commando cmndo2 = new AirCommando("da", "123", new string[5] {"gun","knife","bag","hammer","grenade"}, StatusType.standing);
+            Commando cmndo3 = new SeaCommando("vid", "123", new string[5] {"gun","knife","bag","hammer","grenade"}, StatusType.standing);
+            Commando[] cmndoARR = {cmndo1, cmndo2, cmndo3};
+
+            foreach(Commando cmndo in cmndoARR)
+            {
+                cmndo.Attack();
+            }
+
+            //cmndo.Attack();
+            //cmndo.Walk();
+            //Weapon wpn = new Weapon("s100", "elvit", 5);
+            //wpn.Shoot();
+            //cmndo.CodeName = "456";
         }
     }
 }
